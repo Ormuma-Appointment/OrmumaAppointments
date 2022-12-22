cec961e0f33a16e034abaa972f955570c8981fb9
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-22 10:52:04 +0100

    removed sizes (irrelevant) and added hover

--- packages/ui/components/Link/Link.js
+++ packages/ui/components/Link/Link.js
@@ -1,19 +1,15 @@
 import React from "react";
 import styles from "./Link.module.css";
 import "../../../../apps/web/globals.css";
-import * as PropTypes from "prop-types";
 
 function Link(props) {
-  const { size = "medium", children = "Click here", ...rest } = props;
+  const { children = "Click here", ...rest } = props;
 
   return (
-    <a className={`${styles.link} ${styles[size]}`} {...rest}>
+    <a className={`${styles.link}`} {...rest}>
       {children}
     </a>
   );
 }
-Link.propTypes = {
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-};
 
 export default Link;
--- packages/ui/components/Link/Link.stories.js
+++ packages/ui/components/Link/Link.stories.js
@@ -5,14 +5,9 @@ export default {
   title: "Elements/Link",
   component: Link,
   args: {
-    size: "medium",
     children: "this is a text",
   },
 };
 
 const Template = (args) => <Link {...args} />;
-
 export const PrimaryLink = Template.bind({});
-PrimaryLink.args = {
-  size: "medium",
-};

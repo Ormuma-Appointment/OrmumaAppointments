02df68d6249894a2122c2b91fe4844dc30dfffe0
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-22 11:13:10 +0100

    removed components that are not relevant to the issue

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Button } from "ui";
+import Link from "../../../packages/ui/components/Link/Link";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
       <p className="text-3xl text-red-700 underline">Hello World!</p>
+      <Link>this is a text</Link>
     </div>
   );
 }

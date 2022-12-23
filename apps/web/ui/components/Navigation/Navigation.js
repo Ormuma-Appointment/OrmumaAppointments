5535d644b575f6a106b7aea63437b08db48616c5
MarianneH1 <marianne.helbig@googlemail.com>
2022-12-23 11:51:55 +0100

    added icons for all clickables, added dummy logo

--- apps/web/ui/components/Navigation/Navigation.js
+++ apps/web/ui/components/Navigation/Navigation.js
@@ -2,18 +2,20 @@ import React from "react";
 import styles from "./Navigation.module.css";
 import logout from "../assets/logout.svg";
 import account from "../assets/account.svg";
+import calendar from "../assets/calendar.svg";
+import logo from "../assets/placeholderLogo.png";
 
 function Navigation() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>logoimage</div>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" className={styles.logoImage} />
+      </div>
       <div className={styles.right}>
-        <div className={styles.links}>
-          Calendar
-          <img src={account} alt="account" className={styles.icon} />
-        </div>
         <div className={styles.buttons}>
           <button> Add Appointment </button>
+          <img src={calendar} alt="calendar" className={styles.icon} />
+          <img src={account} alt="account" className={styles.icon} />
           <img src={logout} alt="logout" className={styles.icon} />
         </div>
       </div>

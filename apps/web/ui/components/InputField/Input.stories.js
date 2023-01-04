626a6dd045bf04c091ac975d3910362765bbcab4
orianemgn <mgnoriane@gmail.com>
2023-01-04 18:10:13 +0100

    not working with icon

---
+++ apps/web/ui/components/InputField/Input.stories.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Input from "./Input";
+
+export default {
+  title: "Elements/Input", // should be unique in the whole project
+  component: Input,
+  args: {
+    size: "medium_input",
+    icon: false,
+  },
+};
+
+const Template = (args) => <Input {...args} />;
+
+export const SmallInput = Template.bind({});
+SmallInput.args = {
+  size: "small_input",
+};
+
+export const StandardInput = Template.bind({});
+StandardInput.args = {
+  size: "medium_input",
+};
+
+export const IconInput = Template.bind({});
+StandardInput.args = {
+  icon: true,
+};

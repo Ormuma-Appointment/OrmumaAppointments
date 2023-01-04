8f33583f7107700eb80b86d1a435efa0fcdc48fa
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-04 15:44:23 +0100

    removed folder

--- apps/web/pages/register.jsx
+++ apps/web/pages/register.jsx
@@ -2,10 +2,6 @@ import React, { useState } from "react";
 import Registration from "../ui/components/Registration/Registration";
 
 function register() {
-  const [email, setEmail] = useState(null);
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
   return (
     <>
       <Registration />

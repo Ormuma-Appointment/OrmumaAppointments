db5c4ee3b143ddfc669749eb6b0107c04b5986f9
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-04 20:17:53 +0100

    made initial navbar available globally via _app.tsx

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from "next/app";
+import React from "react";
 import "../globals.css";
+import Navigation from "../ui/components/Navigation/Navigation";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navigation customer_logged_out />
+      <Component {...pageProps} />
+    </>
+  );
 }

d8a2daca9aaf5b6a1c5254588648e3240fcada0c
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-05 19:49:03 +0100

    added props

--- apps/web/ui/components/Contact/Contact.js
+++ apps/web/ui/components/Contact/Contact.js
@@ -3,16 +3,18 @@ import styles from "./Contact.module.css";
 import CardContainer from "../CardContainer/CardContainer";
 import InfoElement from "../InfoElement/InfoElement";
 
-function Contact() {
+function Contact({
+  email = "naturfriseur@gmail.com",
+  website = "www.naturfriseur-aachen.de",
+  telephone = "+49 1577 37384273",
+}) {
   return (
     <CardContainer>
-      <InfoElement email infoDetail="naturfriseur@gmail.com" infoHl="Email" />
-      <InfoElement
-        infoDetail="www.naturfriseur-aachen.de"
-        infoHl="Website"
-        internet
-      />
-      <InfoElement infoDetail="+49 1577 37384273" infoHl="Telefon" phone />
+      <div className={styles.container}>
+        <InfoElement email infoDetail={email} infoHl="Email" />
+        <InfoElement infoDetail={website} infoHl="Website" internet />
+        <InfoElement infoDetail={telephone} infoHl="Telefon" phone />
+      </div>
     </CardContainer>
   );
 }

ec18c0b44ef20d3e1663bfa519df312c252555b7
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-06 16:57:15 +0100

    removed obsolete args from components

--- apps/web/ui/components/CheckboxSelectElement/CheckboxSelectElement.stories.js
+++ apps/web/ui/components/CheckboxSelectElement/CheckboxSelectElement.stories.js
@@ -4,9 +4,6 @@ import CheckboxSelectElement from "./CheckboxSelectElement";
 export default {
   title: "Elements/CheckboxSelectElement",
   component: CheckboxSelectElement,
-  args: {
-    labels: ["hi", "bye"],
-  },
 };
 
 const Template = (args) => <CheckboxSelectElement {...args} />;
@@ -14,5 +11,4 @@ const Template = (args) => <CheckboxSelectElement {...args} />;
 export const HorizontalCheckbox = Template.bind({});
 HorizontalCheckbox.args = {
   labels: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
-  type: "checkbox",
 };

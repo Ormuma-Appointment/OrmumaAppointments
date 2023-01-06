0d92426f110f1511d215615d0731ba778a99c8e4
mulybirish <mulugata13@gmail.com>
2023-01-06 15:41:55 +0100

    r to R in registration function

--- apps/web/firebase/firebase.js
+++ apps/web/firebase/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 

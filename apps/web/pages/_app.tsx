d7a74805c087d2544e8415c54c769cef5b3fd691
mulybirish <mulugata13@gmail.com>
2023-01-07 17:18:05 +0100

    added login with firebase

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from "next/app";
 import "../globals.css";
+import { AuthContextProvider } from "../firebase/AuthContext";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }

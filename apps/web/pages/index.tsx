a432a99da15a3a30b3a44ecd9b60564afd893a99
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 17:40:48 +0100

    add select dropdown for time select as component

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,9 @@
+import SelectElement from "../ui/components/SelectElement/SelectElement";
+
+export default function Web() {
+  return (
+    <div>
+      <SelectElement hour />
+    </div>
+  );
+}

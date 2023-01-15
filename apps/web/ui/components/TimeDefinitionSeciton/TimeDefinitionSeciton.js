82cf16e8791bc23074141159b5e55971186083df
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 16:26:25 +0100

    add files

---
+++ apps/web/ui/components/TimeDefinitionSeciton/TimeDefinitionSeciton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./TimeDefinitionSeciton.module.css";
+
+function TimeDefinitionSeciton({
+  openDays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"],
+}) {
+  return <div>TimeDefinitionSeciton</div>;
+}
+
+export default TimeDefinitionSeciton;

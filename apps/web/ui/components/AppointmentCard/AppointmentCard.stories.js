159e0c423492c7bd3fff8a70f8ad29da0d9db195
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 16:12:29 +0100

    finalize storybook implementation

--- apps/web/ui/components/AppointmentCard/AppointmentCard.stories.js
+++ apps/web/ui/components/AppointmentCard/AppointmentCard.stories.js
@@ -4,7 +4,20 @@ import AppointmentCard from "./AppointmentCard";
 export default {
   title: "Components/AppointmentCard",
   component: AppointmentCard,
+  args: {
+    date: "03.01.2023",
+    time: "11:30-12:00",
+    stylist: "Jochen Lambatz",
+    customer: "Andrea Berg",
+    service: "Haar kurz, schneiden, waschen",
+    cancel: true,
+  },
 };
 
 const Template = (args) => <AppointmentCard {...args} />;
-export const AppointmentCardBookAgain = Template.bind({});
+export const BookAgainAppointment = Template.bind({});
+BookAgainAppointment.args = {
+  cancel: false,
+};
+
+export const CancelAppointment = Template.bind({});

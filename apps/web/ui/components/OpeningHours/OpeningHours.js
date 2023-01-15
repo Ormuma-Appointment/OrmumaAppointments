2a5a0e9f94d0407f9c2c16aee67ee2422ab3313f
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 19:52:22 +0100

    resolve unused kex prop issue, remove console.log in OpeningHours

--- apps/web/ui/components/OpeningHours/OpeningHours.js
+++ apps/web/ui/components/OpeningHours/OpeningHours.js
@@ -3,14 +3,13 @@ import styles from "./OpeningHours.module.css";
 import CardContainer from "../CardContainer/CardContainer";
 
 function OpeningHours({ hours }) {
-  console.log(hours);
   return (
     <CardContainer>
       <div className={styles.container}>
         <h3>Öffnungszeiten</h3>
         {hours.map((el, index) => {
           return (
-            <div className={styles.day}>
+            <div key={index} className={styles.day}>
               <p>{el.day}</p>{" "}
               <div>
                 <p>{el.start}</p> - <p> {el.end}</p>

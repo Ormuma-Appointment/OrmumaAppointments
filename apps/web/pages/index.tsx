4a65237073c82136a0fe92333702dce0c8bab619
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 14:59:58 +0100

    add styling

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,31 +1,3 @@
-import Navigation from "../ui/components/Navigation/Navigation";
-import InfoElement from "../ui/components/InfoElement/InfoElement";
-import AddressCard from "../ui/components/AddressCard/AddressCard";
-
 export default function Web() {
-  return (
-    <div>
-      <Navigation admin_logged_in />
-      <h1>Web</h1>
-      <p>Hello World!</p>
-      <div>
-        <InfoElement email infoDetail="naturfriseur@gmail.com" infoHl="Email" />
-        <InfoElement
-          infoDetail="www.naturfriseur-aachen.de"
-          infoHl="Website"
-          internet
-        />
-        <InfoElement infoDetail="+49 1577 37384273" infoHl="Telefon" phone />
-        <AddressCard
-          name="Naturfriseur Aachen"
-          street="Habsburgerallee"
-          number={11}
-          postalCode="52064"
-          city="Aachen"
-          country="Deutschland"
-        />
-      </div>
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }

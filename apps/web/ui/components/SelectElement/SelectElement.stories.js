fe5a38f5ba570be87e4020718936ae66a19f026a
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 17:15:14 +0100

    add selectElement functionality

---
+++ apps/web/ui/components/SelectElement/SelectElement.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SelectElement from "./SelectElement";
+
+export default {
+  title: "Elements/SelectElement",
+  component: SelectElement,
+};
+
+const Template = (args) => <SelectElement {...args} />;
+export const PrimaryLink = Template.bind({});

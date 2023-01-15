d43da9d4fffda517b372352c880725ad69c6a748
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 18:26:51 +0100

    adapt OpeningHours to use props

--- apps/web/ui/components/OpeningHours/OpeningHours.stories.js
+++ apps/web/ui/components/OpeningHours/OpeningHours.stories.js
@@ -3,7 +3,16 @@ import OpeningHours from "./OpeningHours";
 export default {
   title: "Components/OpeningHours",
   component: OpeningHours,
-  args: {},
+  args: {
+    hours: [
+      { day: "Monday", start: "08:00", end: "18:00" },
+      { day: "Tuesday", start: "08:00", end: "18:00" },
+      { day: "Wednesday", start: "08:00", end: "18:00" },
+      { day: "Thursday", start: "08:00", end: "18:00" },
+      { day: "Friday", start: "08:00", end: "18:00" },
+      { day: "Saturday", start: "08:00", end: "15:00" },
+    ],
+  },
 };
 
 const Template = (args) => <OpeningHours {...args} />;

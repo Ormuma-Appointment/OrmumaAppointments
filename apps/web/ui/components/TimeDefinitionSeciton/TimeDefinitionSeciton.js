264ddc98e9674ab3b331473599bdb748da459249
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 16:29:25 +0100

    add map

--- apps/web/ui/components/TimeDefinitionSeciton/TimeDefinitionSeciton.js
+++ apps/web/ui/components/TimeDefinitionSeciton/TimeDefinitionSeciton.js
@@ -4,7 +4,13 @@ import styles from "./TimeDefinitionSeciton.module.css";
 function TimeDefinitionSeciton({
   openDays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"],
 }) {
-  return <div>TimeDefinitionSeciton</div>;
+  return (
+    <div className={styles.container}>
+      {openDays.map((el, index) => {
+        return <div key={index}>{el} </div>;
+      })}
+    </div>
+  );
 }
 
 export default TimeDefinitionSeciton;

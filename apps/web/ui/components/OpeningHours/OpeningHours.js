aa55ebe5c0f319cea8890c5f0c7f9c4693e607a9
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 15:01:20 +0100

    add more dummy data

--- apps/web/ui/components/OpeningHours/OpeningHours.js
+++ apps/web/ui/components/OpeningHours/OpeningHours.js
@@ -6,6 +6,10 @@ function OpeningHours({
   hours = [
     { day: "Monday", start: "08:00", end: "18:00" },
     { day: "Tuesday", start: "08:00", end: "18:00" },
+    { day: "Wednesday", start: "08:00", end: "18:00" },
+    { day: "Thursday", start: "08:00", end: "18:00" },
+    { day: "Friday", start: "08:00", end: "18:00" },
+    { day: "Saturday", start: "08:00", end: "15:00" },
   ],
 }) {
   console.log(hours);

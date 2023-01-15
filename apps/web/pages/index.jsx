5cace65bafde2c433c431b310cb8bebc5d07624f
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 18:18:36 +0100

    add round image component

--- apps/web/pages/index.jsx
+++ apps/web/pages/index.jsx
@@ -1,7 +1,13 @@
+import RoundImage from "../ui/components/RoundImage/RoundImage";
 export default function Web() {
   return (
     <div>
       <h1>Wir lieben Natürlichkeit</h1>
+      <RoundImage
+        alt="Nice Image"
+        image="https://images.unsplash.com/photo-1441829266145-6d4bfbd38eb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VhfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+        initialWidth={60}
+      />
     </div>
   );
 }

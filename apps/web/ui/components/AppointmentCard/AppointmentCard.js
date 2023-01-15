ff69c5f8fd9b27250e54375a9ff76198473906d6
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 16:06:18 +0100

    add initial styling

--- apps/web/ui/components/AppointmentCard/AppointmentCard.js
+++ apps/web/ui/components/AppointmentCard/AppointmentCard.js
@@ -2,17 +2,43 @@ import React from "react";
 import styles from "./AppointmentCard.module.css";
 import CardContainer from "../CardContainer/CardContainer";
 import Person from "../assets/account.svg";
+import Cut from "../assets/scissors.svg";
+import Button from "../Button/Button";
 
-function AppointmentCard() {
+function AppointmentCard({
+  date = "03.01.2023",
+  time = "11:30-12:00",
+  stylist = "Jochen Lambatz",
+  customer = "Andrea Berg",
+  service = "Haar kurz, schneiden, waschen",
+  cancel,
+}) {
   return (
     <CardContainer>
       <div className={styles.container}>
-        <div className={styles.header}></div>
-        <div className={styles.person}>
-          <Person className={styles.icon} />
+        <div className={styles.header}>
+          <div className={styles.time}>
+            {date} / {time}
+          </div>
+          {cancel ? (
+            <Button icon="" size="xsmall" variant="danger">
+              absagen
+            </Button>
+          ) : (
+            <Button icon="" size="xsmall" variant="primary">
+              erneut buchen
+            </Button>
+          )}
         </div>
-        <div className={styles.service}>
+        <div className={styles.info}>
           <Person className={styles.icon} />
+          {stylist && <p>{stylist}</p>}
+          {stylist && customer && <>/</>}
+          {customer && <p>{customer}</p>}
+        </div>
+        <div className={styles.info}>
+          <Cut className={styles.icon} />
+          <p>{service}</p>
         </div>
       </div>
     </CardContainer>

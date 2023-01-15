46391cbc277d9e577104b258d209e3540774bf67
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 18:29:04 +0100

    adapt ContactCard to use props

--- apps/web/pages/index.jsx
+++ apps/web/pages/index.jsx
@@ -1,6 +1,7 @@
 import RoundImage from "../ui/components/RoundImage/RoundImage";
 import Button from "../ui/components/Button/Button";
 import Logo from "../ui/components/assets/placeholderLogo.png";
+import OpeningHours from "../ui/components/OpeningHours/OpeningHours";
 
 export default function Web() {
   return (
@@ -14,6 +15,42 @@ export default function Web() {
       </div>
       <div>
         <h2>Mehr über uns</h2>
+        <div>
+          <OpeningHours
+            hours={[
+              {
+                day: "Monday",
+                end: "18:00",
+                start: "08:00",
+              },
+              {
+                day: "Tuesday",
+                end: "18:00",
+                start: "08:00",
+              },
+              {
+                day: "Wednesday",
+                end: "18:00",
+                start: "08:00",
+              },
+              {
+                day: "Thursday",
+                end: "18:00",
+                start: "08:00",
+              },
+              {
+                day: "Friday",
+                end: "18:00",
+                start: "08:00",
+              },
+              {
+                day: "Saturday",
+                end: "15:00",
+                start: "08:00",
+              },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
--- apps/web/ui/components/ContactCard/ContactCard.js
+++ apps/web/ui/components/ContactCard/ContactCard.js
@@ -3,11 +3,7 @@ import styles from "./ContactCard.module.css";
 import CardContainer from "../CardContainer/CardContainer";
 import InfoElement from "../InfoElement/InfoElement";
 
-function ContactCard({
-  email = "naturfriseur@gmail.com",
-  website = "www.naturfriseur-aachen.de",
-  telephone = "+49 1577 37384273",
-}) {
+function ContactCard({ email, website, telephone }) {
   return (
     <CardContainer>
       <div className={styles.container}>

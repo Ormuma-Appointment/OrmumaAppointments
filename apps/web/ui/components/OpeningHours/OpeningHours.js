58e2e73be419e3017e33a0463ead3db08ea63365
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 14:59:58 +0100

    add styling

--- apps/web/ui/components/OpeningHours/OpeningHours.js
+++ apps/web/ui/components/OpeningHours/OpeningHours.js
@@ -2,20 +2,24 @@ import React from "react";
 import styles from "./OpeningHours.module.css";
 import CardContainer from "../CardContainer/CardContainer";
 
-function OpeningHours(
-  openingHours = [
+function OpeningHours({
+  hours = [
     { day: "Monday", start: "08:00", end: "18:00" },
     { day: "Tuesday", start: "08:00", end: "18:00" },
-  ]
-) {
-  console.log(openingHours);
+  ],
+}) {
+  console.log(hours);
   return (
     <CardContainer>
       <div className={styles.container}>
-        {openingHours.map((el, index) => {
+        <h3>Öffnungszeiten</h3>
+        {hours.map((el, index) => {
           return (
-            <div>
-              <p>{el.day}</p>
+            <div className={styles.day}>
+              <p>{el.day}</p>{" "}
+              <div>
+                <p>{el.start}</p> - <p> {el.end}</p>
+              </div>
             </div>
           );
         })}

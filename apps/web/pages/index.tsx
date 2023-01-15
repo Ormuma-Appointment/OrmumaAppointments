a4457e9093a0db69672f940ead30a203df7a5578
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 14:59:58 +0100

    add styling

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,8 +1,3 @@
-import TimeDefinitionSection from "../ui/components/TimeDefinitionSeciton/TimeDefinitionSection";
 export default function Web() {
-  return (
-    <div>
-      <TimeDefinitionSection />
-    </div>
-  );
+  return <div></div>;
 }

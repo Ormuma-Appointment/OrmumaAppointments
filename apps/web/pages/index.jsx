2bb5e9503fb94d4b81c6737ec94d7c6f6025978c
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-15 20:08:14 +0100

    add dummy data to homepage

--- apps/web/pages/index.jsx
+++ apps/web/pages/index.jsx
@@ -8,6 +8,54 @@ import AddressCard from "../ui/components/AddressCard/AddressCard";
 import calendar from "../ui/components/assets/calendar_add.svg";
 
 export default function Web() {
+  const contact = {
+    email: "naturfriseur@gmail.com",
+    telephone: "+49 1577 37384273",
+    website: "www.naturfriseur-aachen.de",
+  };
+
+  const address = {
+    city: "Aachen",
+    country: "Deutschland",
+    name: "Naturfriseur Aachen",
+    number: 11,
+    postalCode: "52064",
+    street: "Habsburgerallee",
+  };
+
+  const openingHours = [
+    {
+      day: "Monday",
+      end: "18:00",
+      start: "08:00",
+    },
+    {
+      day: "Tuesday",
+      end: "18:00",
+      start: "08:00",
+    },
+    {
+      day: "Wednesday",
+      end: "18:00",
+      start: "08:00",
+    },
+    {
+      day: "Thursday",
+      end: "18:00",
+      start: "08:00",
+    },
+    {
+      day: "Friday",
+      end: "18:00",
+      start: "08:00",
+    },
+    {
+      day: "Saturday",
+      end: "15:00",
+      start: "08:00",
+    },
+  ];
+
   function handleBookingClick(e) {
     e.preventDefault();
     console.log("I should go to the booking process");
@@ -30,54 +78,20 @@ export default function Web() {
         <h2>Mehr über uns</h2>
         <div className={styles.info_row}>
           <ContactCard
-            email="naturfriseur@gmail.com"
-            telephone="+49 1577 37384273"
-            website="www.naturfriseur-aachen.de"
+            email={contact.email}
+            telephone={contact.telephone}
+            website={contact.website}
           />
-          <OpeningHours
-            hours={[
-              {
-                day: "Monday",
-                end: "18:00",
-                start: "08:00",
-              },
-              {
-                day: "Tuesday",
-                end: "18:00",
-                start: "08:00",
-              },
-              {
-                day: "Wednesday",
-                end: "18:00",
-                start: "08:00",
-              },
-              {
-                day: "Thursday",
-                end: "18:00",
-                start: "08:00",
-              },
-              {
-                day: "Friday",
-                end: "18:00",
-                start: "08:00",
-              },
-              {
-                day: "Saturday",
-                end: "15:00",
-                start: "08:00",
-              },
-            ]}
-          />
+          <OpeningHours hours={openingHours} />
         </div>
         <div className={styles.info_row}>
           <AddressCard
-            city="Aachen"
-            country="Deutschland"
-            name="Naturfriseur Aachen"
-            number={11}
-            postalCode="52064"
-            street="Habsburgerallee"
-            telephone="0241 / 70 98 99"
+            city={address.city}
+            country={address.country}
+            name={address.name}
+            number={address.number}
+            postalCode={address.postalCode}
+            street={address.street}
           />
         </div>
       </div>

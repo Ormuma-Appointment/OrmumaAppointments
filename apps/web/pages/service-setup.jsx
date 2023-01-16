fe97f1df2db190a46e073fa13aa9f5ede6f5b031
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-16 15:06:14 +0100

    add minus svg, pill styles, primary color in light in global styles

--- apps/web/pages/service-setup.jsx
+++ apps/web/pages/service-setup.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardContainer from "../ui/components/CardContainer/CardContainer";
 import Input from "../ui/components/InputField/Input";
 import Button from "../ui/components/Button/Button";
 import styles from "../ui/page_styles/ServiceSetup.module.css";
+import Minus from "../ui/components/assets/minus.svg";
 
 function ServiceSetup() {
+  const [categories, setCategories] = useState([]);
+  function handleCatSubmit(e) {
+    e.preventDefault();
+    setCategories((prev) => [...prev, e.target.category.value]);
+  }
+
+  useEffect(() => {
+    console.log(categories);
+  }, [categories]);
   return (
     <div>
       <h1>Services Konfigurieren</h1>
@@ -24,11 +34,20 @@ function ServiceSetup() {
         </div>
         <div className={styles.service_cat}>
           <h3>1. Service Kategorien erstellen</h3>
-          <div className={styles.input_group}>
-            <Input placeholder="Kategorie hinzufügen ..." />{" "}
+          <form className={styles.input_group} onSubmit={handleCatSubmit}>
+            <Input placeholder="Kategorie hinzufügen ..." name="category" />{" "}
             <Button icon="" size="medium" variant="primary">
               + hinzufügen
             </Button>
+          </form>
+          <div className={styles.cat_box}>
+            {categories.map((el, index) => {
+              return (
+                <div className={styles.pill}>
+                  {el} <Minus className={styles.icon} />
+                </div>
+              );
+            })}
           </div>
         </div>
       </CardContainer>

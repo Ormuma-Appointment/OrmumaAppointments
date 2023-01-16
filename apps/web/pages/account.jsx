a3bc963e5de74a54feed8742e8c5bbaa4c304bab
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-16 09:00:16 +0100

    add css for initial structure

--- apps/web/pages/account.jsx
+++ apps/web/pages/account.jsx
@@ -1,23 +1,29 @@
 import React from "react";
+import styles from "../ui/page_styles/Account.module.css";
 import Button from "../ui/components/Button/Button";
 import calendar from "../ui/components/assets/calendar.svg";
 
 function account() {
   return (
-    <div>
-      <h1>Mein Account</h1>
-      <Button icon={calendar} size="medium" variant="primary">
-        Termin buchen
-      </Button>
-      <div>
-        <h3>Meine Daten</h3>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Mein Account</h1>
+        <Button icon={calendar} size="medium" variant="primary">
+          Termin buchen
+        </Button>
       </div>
       <div>
-        <div>
+        <h3>Meine Daten</h3>
+        <div className={styles.box}></div>
+      </div>
+      <div className={styles.appointments}>
+        <div className={styles.appointment_box}>
           <h3>Mein nächster Termin</h3>
+          <div className={styles.box}></div>
         </div>
-        <div>
+        <div className={styles.appointment_box}>
           <h3>Mein vergangenen Termin(e)</h3>
+          <div className={styles.box}></div>
         </div>
       </div>
     </div>

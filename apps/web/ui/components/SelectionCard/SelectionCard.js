5c21e8044b302fef39c0d90060f6b118378a145e
orianemgn <mgnoriane@gmail.com>
2023-01-16 17:09:58 +0100

    booking service UI done

---
+++ apps/web/ui/components/SelectionCard/SelectionCard.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styles from "./SelectionCard.module.css";
+import Link from "next/link";
+import SelectItem from "../SelectItem/SelectItem";
+import Button from "../Button/Button";
+
+const SelectionCard = (props) => {
+  const step = props.step;
+  const selectedService = props.selectedService;
+  const setSelectedService = props.setSelectedService;
+  const selectedEmployee = props.selectedEmployee;
+  const setSelectedEmployee = props.setSelectedEmployee;
+  const service = props.service;
+  return (
+    <div>
+      <h4>Ihre Auswahl</h4>
+      {step === "service" && (
+        <>
+          <div>
+            {!selectedService && (
+              <p className={styles.selectItemText}>Select a service</p>
+            )}
+            <div onClick={() => setSelectedService(null)}>
+              {selectedService && (
+                <SelectItem
+                  duration={selectedService.duration}
+                  minus
+                  price={selectedService.price}
+                  title={selectedService.title}
+                />
+              )}
+            </div>
+          </div>
+          <div className={styles.buttonsContainer}>
+            <Button icon="" size="medium" variant="danger">
+              <Link href="/">Go back</Link>
+            </Button>
+
+            {selectedService && (
+              <Button icon="" size="medium" variant="primary">
+                <Link href="/booking-employee">Next step</Link>
+              </Button>
+            )}
+          </div>
+        </>
+      )}
+      {step === "employee" && (
+        <>
+          <div>
+            <ServiceItem service={service} />
+            {!selectedEmployee && (
+              <p className={styles.selectItemText}>Select a employee</p>
+            )}
+            <div onClick={() => setSelectedEmployee(null)}>
+              {selectedEmployee && (
+                <EmployeeItem
+                  employee={selectedEmployee}
+                  icon="fa-solid fa-circle-minus"
+                  danger="danger"
+                />
+              )}
+            </div>
+          </div>
+          <div className={styles.buttonsContainer}>
+            <Button icon="" size="medium" variant="danger">
+              <Link href="/">Go back</Link>
+            </Button>
+
+            {selectedEmployee && (
+              <Button icon="" size="medium" variant="primary">
+                <Link href="/">Next step</Link>
+              </Button>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SelectionCard;

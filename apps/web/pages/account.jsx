d0b5223066ba663db8977131ea73c1f920b63373
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-16 08:50:39 +0100

    add initial page structure

--- apps/web/pages/account.jsx
+++ apps/web/pages/account.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function account() {
-  return <div>account</div>;
+  return (
+    <div>
+      <h1>Mein Account</h1>
+      <div>
+        <h3>Meine Daten</h3>
+      </div>
+      <div>
+        <div>
+          <h3>Mein nächster Termin</h3>
+        </div>
+        <div>
+          <h3>Mein vergangenen Termin(e)</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default account;

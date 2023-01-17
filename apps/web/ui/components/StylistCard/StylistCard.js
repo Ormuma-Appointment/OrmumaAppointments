0c190efb153de72b529157abf6bf680d0c0b03f9
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-17 10:35:15 +0100

    add responsive css

--- apps/web/ui/components/StylistCard/StylistCard.js
+++ apps/web/ui/components/StylistCard/StylistCard.js
@@ -2,22 +2,16 @@ import React from "react";
 import styles from "./StylistCard.module.css";
 import CardContainer from "../CardContainer/CardContainer";
 import RoundImage from "../RoundImage/RoundImage";
-import placeHolder from "../assets/placeholder-profile.jpeg";
 
-function StylistCard({
-  image = placeHolder,
-  name = "Sammy Laury",
-  text = "Spezialisiert in Balayage und vibrant colors",
-}) {
+function StylistCard({ image, name, text }) {
   return (
     <CardContainer>
       <div className={styles.container}>
         <div className={styles.image}>
           <RoundImage alt={name} image={image} initialWidth={10} />
         </div>
-
-        <div>
-          <div className={styles.name}>{name}</div>
+        <div className={styles.info}>
+          <h3 className={styles.name}>{name}</h3>
           <div className={styles.text}>{text}</div>
         </div>
       </div>

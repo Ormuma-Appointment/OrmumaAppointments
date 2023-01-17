e5bd419dc9ae490e5f7dca48c3de1f14e3fc3ee0
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-17 10:17:42 +0100

    add RoundImage

--- apps/web/ui/components/StylistCard/StylistCard.js
+++ apps/web/ui/components/StylistCard/StylistCard.js
@@ -1,9 +1,28 @@
 import React from "react";
 import styles from "./StylistCard.module.css";
 import CardContainer from "../CardContainer/CardContainer";
+import RoundImage from "../RoundImage/RoundImage";
+import placeHolder from "../assets/placeholder-profile.jpeg";
 
-function StylistCard() {
-  return <CardContainer>StylistCard</CardContainer>;
+function StylistCard({
+  image = placeHolder,
+  name = "Sammy Laury",
+  text = "Spezialisiert in Balayage und vibrant colors",
+}) {
+  return (
+    <CardContainer>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <RoundImage alt={name} image={image} initialWidth={10} />
+        </div>
+
+        <div>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.text}>{text}</div>
+        </div>
+      </div>
+    </CardContainer>
+  );
 }
 
 export default StylistCard;

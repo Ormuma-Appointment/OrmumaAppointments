477cb466f238e0bc308e25767a94a692bb83b8a7
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-17 16:26:14 +0100

    start to get values from Select

--- apps/web/ui/components/TimeDefinitionSection/TimeDefinitionSection.js
+++ apps/web/ui/components/TimeDefinitionSection/TimeDefinitionSection.js
@@ -2,17 +2,20 @@ import React from "react";
 import styles from "./TimeDefinitionSection.module.css";
 import SelectElement from "../SelectElement/SelectElement";
 
-function TimeDefinitionSection({ openDays }) {
+function TimeDefinitionSection({ openDays, setTimes }) {
+  function handleChange(e) {
+    console.log(e.target.name);
+  }
   return (
     <div className={styles.container}>
       {openDays.map((el, index) => {
         return (
           <div key={index}>
-            <div className={styles.input_group}>
-              <p>{el}</p> <SelectElement placeholder="Start" />
-              <SelectElement placeholder="Ende" />
-              <p>Pause</p> <SelectElement placeholder="Start" />
-              <SelectElement placeholder="Ende" />
+            <div className={styles.input_group} onChange={handleChange}>
+              <p>{el}</p> <SelectElement placeholder="Start" day={el} />
+              <SelectElement placeholder="Ende" day={el} />
+              <p>Pause</p> <SelectElement placeholder="Start" day={el} />
+              <SelectElement placeholder="Ende" day={el} />
             </div>
           </div>
         );

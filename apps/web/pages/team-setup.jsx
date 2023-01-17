69f5c16729194bd5d23a1654a669cf3b1a08babd
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-17 11:24:43 +0100

    add employee info and working hours

--- apps/web/pages/team-setup.jsx
+++ apps/web/pages/team-setup.jsx
@@ -21,7 +21,107 @@ function TeamSetup() {
         <span className={styles.arrows}>&#9654;</span>
       </div>
       <h1>Store Setup</h1>
-      <CardContainer></CardContainer>
+      <CardContainer>
+        <div className={styles.container}>
+          <form action="">
+            <div className={styles.intro}>
+              To add a salon employee fill out the fields below and click on
+              add. The employee will appear below. None of the personal
+              information will be visible to the customers.
+            </div>
+            <div className={styles.form}>
+              <div className={styles.setUpInfos}>
+                <div className={styles.row}>
+                  <div className={styles.col30}>
+                    <label>Name:*</label>
+                  </div>
+                  <div className={styles.col70}>
+                    <Input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="Salon Name"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className={`${styles.row} ${styles.adresse}`}>
+                  <div className={styles.col30}>
+                    <label>Adresse:*</label>
+                  </div>
+                  <div className={styles.col70}>
+                    <Input
+                      type="text"
+                      name="street"
+                      id="street"
+                      placeholder="Straße, Nummer"
+                    />
+                    <div className={`${styles.row} ${styles.city}`}>
+                      <div className={styles.col50}>
+                        <Input
+                          type="text"
+                          name="postalCode"
+                          id="postalCode"
+                          placeholder="Postleitzahl"
+                        />
+                      </div>
+                      <div className={styles.col50}>
+                        <Input
+                          type="text"
+                          name="city"
+                          id="city"
+                          placeholder="Stadt"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.row}>
+                  <div className={styles.col30}>
+                    <label>Telefon:*</label>
+                  </div>
+                  <div className={styles.col70}>
+                    <Input
+                      type="text"
+                      name="phone"
+                      id="phone"
+                      placeholder="Telefonnummer"
+                    />
+                  </div>
+                </div>
+                <div className={styles.row}>
+                  <div className={styles.col30}>
+                    <label>Stylistenfoto:*</label>
+                  </div>
+                  <div className={styles.col70}>
+                    <Input type="file" name="logo" id="logo" required />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.setUpOpenings}>
+              <div className={`${styles.row} ${styles.opening}`}>
+                <div className={styles.col30}>
+                  <label>Öffnungstage:*</label>
+                </div>
+                <div className={styles.col70}>
+                  <CheckboxSelectElement
+                    labels={["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]}
+                  />
+                </div>
+              </div>
+              <div className={`${styles.row} ${styles.opening}`}>
+                <div className={styles.col30}>
+                  <label>Öffnungszeiten:*</label>
+                </div>
+                <div className={styles.col70}>
+                  <TimeDefinitionSection />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </CardContainer>
     </div>
   );
 }

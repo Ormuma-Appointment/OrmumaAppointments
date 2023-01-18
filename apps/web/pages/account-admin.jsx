2f87825002f1986b634984e6e34b6e56979ace6a
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-18 11:45:56 +0100

    add address

--- apps/web/pages/account-admin.jsx
+++ apps/web/pages/account-admin.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import styles from "../ui/page_styles/Account.module.css";
+import styles from "../ui/page_styles/AccountAdmin.module.css";
 import Button from "../ui/components/Button/Button";
 import calendar from "../ui/components/assets/calendar.svg";
 import AccountCard from "../ui/components/AccountCard/AccountCard";
 import AppointmentCard from "../ui/components/AppointmentCard/AppointmentCard";
+import CardContainer from "../ui/components/CardContainer/CardContainer";
 
 const AccountAdmin = () => {
-  const pastAppointments = [
+  const salon = {
+    name: "Naturfriseur Aachen",
+    street: "Habsburgerallee 11",
+    postalcode: "52064",
+    city: "Aachen",
+    country: "Deutschland",
+  };
+  const todaysAppointments = [
     {
       customer: "Andrea Berg",
       date: "03.01.2023",
@@ -16,19 +24,27 @@ const AccountAdmin = () => {
     },
   ];
 
-  console.log(currentUser);
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h1>Mein Account</h1>
-        <Button icon={calendar} size="medium" variant="primary">
-          Termin buchen
-        </Button>
       </div>
-      <div>
-        <h3>Meine Daten</h3>
-        <AccountCard className={styles.box}></AccountCard>
+      <div className={styles.row}>
+        <h3>Salon Übersicht</h3>
+        <CardContainer>
+          <div className={styles.container}>
+            <div className={styles.address}>
+              <ul>
+                <li>{salon.name}</li>
+                <li>{salon.street}</li>
+                <li>
+                  {salon.postalCode} {salon.city}
+                </li>
+                <li>{salon.country}</li>
+              </ul>
+            </div>
+          </div>
+        </CardContainer>
       </div>
       <div className={styles.appointments}>
         <div className={styles.appointment_box}>
@@ -45,7 +61,7 @@ const AccountAdmin = () => {
         <div className={styles.appointment_box}>
           <h3>Mein vergangenen Termin(e)</h3>
           <div>
-            {pastAppointments.map((el, index) => {
+            {todaysAppointments.map((el, index) => {
               return (
                 <AppointmentCard
                   customer={el.name}
@@ -64,4 +80,4 @@ const AccountAdmin = () => {
   // }
 };
 
-export default Account;
+export default AccountAdmin;

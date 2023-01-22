27c9fa99ea9607e7998a4e66029e1b6424ffb440
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-22 12:34:02 +0100

    add firebase data for categories and service labels

--- apps/web/pages/service-setup.jsx
+++ apps/web/pages/service-setup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import CardContainer from "../ui/components/CardContainer/CardContainer";
 import Input from "../ui/components/InputField/Input";
@@ -7,14 +7,53 @@ import styles from "../ui/page_styles/ServiceSetup.module.css";
 import ServiceAdd from "../ui/components/ServiceAdd/ServiceAdd";
 import Minus from "../ui/components/assets/minus.svg";
 import Link from "next/link";
+<<<<<<< HEAD
 // import { doc, setDoc, collection } from "firebase/firestore";
 import { db } from "../firebase/firebase";
 import { doc, setDoc, collection, query, getDocs } from "firebase/firestore";
+=======
+import { db } from "../firebase/firebase";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  collection,
+} from "firebase/firestore";
+import { useAuthContext } from "../context/AuthContext";
+>>>>>>> 11966fc (add firebase data for categories and service labels)
 
 function ServiceSetup() {
   const [categories, setCategories] = useState([]);
   const [services, setServices] = useState([]);
   const [data, setData] = useState([]);
+  const { currentUser } = useAuthContext();
+
+  const [dbServices, setDbServices] = useState([]);
+  async function getDBServices() {
+    if (currentUser) {
+      const docRef = doc(db, "stores", "one", "services", "all");
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data().serviceObj);
+        let data = docSnap.data().serviceObj;
+        setCategories(() => data.map((el) => el.category));
+        setServices(
+          () => data.map((el) => el.services.map((elem) => elem.service))[0]
+        );
+        setDbServices(data);
+      }
+    } else {
+      console.log("No such document!");
+    }
+  }
+
+  useEffect(() => {
+    getDBServices();
+  }, [currentUser]);
+  useEffect(() => {
+    console.log(services);
+  }, [services]);
 
   function handleCatSubmit(e) {
     e.preventDefault();

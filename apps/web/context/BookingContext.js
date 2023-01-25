7f3fa811effefcdb65a8d930f2e176392fa1bcc1
orianemgn <mgnoriane@gmail.com>
2023-01-25 14:16:34 +0100

    selected service working

--- apps/web/context/BookingContext.js
+++ apps/web/context/BookingContext.js
@@ -4,9 +4,27 @@ import { collection, doc, setDoc, getDoc, getDocs } from "firebase/firestore";
 
 export const BookingContext = createContext();
 export const BookingContextProvider = ({ children }) => {
+  const [serviceList, setServiceList] = useState({});
   const [employeeData, setEmployeeData] = useState([]);
+  const [chosenService, setChosenService] = useState(null);
   const [chosen, setChosen] = useState(null);
   const [isLoading, SetIsLoading] = useState(true);
+
+  async function getData() {
+    const docRef = doc(db, "stores", "one", "services", "serviceList");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setServiceList(docSnap.data());
+      SetIsLoading(false);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
 
   const handleRead = async () => {
     const docRef = collection(db, "stores", "one", "employeeList");
@@ -27,7 +45,15 @@ export const BookingContextProvider = ({ children }) => {
 
   return (
     <BookingContext.Provider
-      value={{ employeeData, setChosen, chosen, isLoading }}
+      value={{
+        serviceList,
+        employeeData,
+        chosenService,
+        setChosenService,
+        setChosen,
+        chosen,
+        isLoading,
+      }}
     >
       {children}
     </BookingContext.Provider>

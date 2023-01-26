e46a8e3ad8cc0477801b5e57cbf1fcfed8b8ac43
orianemgn <mgnoriane@gmail.com>
2023-01-26 17:11:11 +0100

    button calendar and logic for the booking that we have to do for the next steps

--- apps/web/pages/booking-confirmation.js
+++ apps/web/pages/booking-confirmation.js
@@ -20,7 +20,45 @@ const BookingConfirmation = () => {
 
   const router = useRouter();
 
-  let event = { ...chosen, ...chosenService, ...chosenSlot };
+  let event = { ...chosen, ...chosenService, ...chosenSlot }; // => have to go to collection events
+
+  /*1. We have to send the event inside collection events
+  looking like that 
+  events =  [
+    {
+      category:  "long hair",
+      date: Tue Jan 24 2023 00:00:00 GMT+0100,
+      duration : 45, 
+      employee: "Signe Cervantes",
+      end: "11:45",
+      price: 2,
+      service:"cut",
+      slot:['11:00', '11:45'],
+      start: "11:00" 
+    }
+  ]
+  2. We have to send the date and the slot inside the employee appointements:
+   inside collection employee add something loking like : 
+  appointment : {}
+  when I have a new event => 
+  appointments : {
+    date: [[startTime, endTime]]
+  }
+  if new event at the same date = >
+  appointments : {
+    date: [[startTime, endTime], [startTime, endTime]]
+  }
+  if new event at a new date : 
+  appointments : {
+    date: [[startTime, endTime]], 
+    date2: [[startTime, endTime]]
+  }
+
+  3. when we are in time for now we have the working time and break time
+  check in the appointments if already have an appointment/ several appointement at this date
+  if yes then add the slot inside the breakTime
+  if no just to like we did before
+  }*/
 
   console.log("EVENNNT", event);
 

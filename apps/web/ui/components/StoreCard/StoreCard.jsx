1574238009d830e680c675e4ec80351074bc988f
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-27 21:03:13 +0100

    add upload, stark working on download. CORS issues stop process

--- apps/web/ui/components/StoreCard/StoreCard.jsx
+++ apps/web/ui/components/StoreCard/StoreCard.jsx
@@ -1,18 +1,40 @@
-import React from "react";
-import Button from "../Button/Button";
+import React, { useEffect } from "react";
 import styles from "./StoreCard.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import placeholder from "../assets/salon-placeholder.jpeg";
+import { storage } from "../../../firebase/firebase";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 function StoreCard({ data }) {
+  useEffect(() => {
+    const imageLocation = ref(storage, `images/${data.id}`);
+
+    getDownloadURL(ref(imageLocation))
+      .then((url) => {
+        // `url` is the download URL for 'images/stars.jpg'
+
+        // This can be downloaded directly:
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "blob";
+        xhr.onload = (event) => {
+          const blob = xhr.response;
+        };
+        xhr.open("GET", url);
+        xhr.send();
+
+        // Or inserted into an <img> element
+        const img = document.getElementById("myimg");
+        img.setAttribute("src", url);
+      })
+      .catch((error) => {
+        // Handle any errors
+      });
+  }, []);
   return (
     <div className={`container ${styles.container}`}>
       <Link href={`/h/${data.data.slug}`}>
-        <Image
-          src={data.data.photo ? data.data.photo : placeholder}
-          alt={`Salon: ${data.data.name}`}
-        ></Image>
+        <Image src={placeholder} alt={`Salon: ${data.data.name}`}></Image>
         <div className={styles.store}>
           <h3>{data.data.name}</h3>
           <div className={styles.address}>

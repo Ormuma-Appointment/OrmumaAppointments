56aa4d76c11def5dac22f0d1f091732ca88eb991
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-14 06:23:54 +0100

    remove exidentally copied files from commit 783b53c0

--- ui/components/CardContainer/CardContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./CardContainer.module.css";
-
-function CardContainer(props) {
-  const { children, ...rest } = props;
-  return <div className={styles.container}>{children}</div>;
-}
-
-export default CardContainer;

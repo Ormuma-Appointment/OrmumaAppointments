1a14b4e04850621256a3ebc496c566ebce669525
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-17 15:48:23 +0100

    declare svg fileformat

--- apps/web/ui/index.tsx
+++ apps/web/ui/index.tsx
@@ -1,2 +1,10 @@
 import * as React from "react";
 export * from "./Button";
+
+declare module "*.svg" {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+
+  export { ReactComponent };
+  export default content;
+}

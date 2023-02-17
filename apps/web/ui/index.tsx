f973cf672dd24e99d3797db5de3a0cb48ec600eb
MarianneH1 <marianne.helbig@googlemail.com>
2023-02-17 15:49:21 +0100

    remove irrelevant lined from index.ts ts file

--- apps/web/ui/index.tsx
+++ apps/web/ui/index.tsx
@@ -1,10 +1,2 @@
 import * as React from "react";
 export * from "./Button";
-
-declare module "*.svg" {
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const content: string;
-
-  export { ReactComponent };
-  export default content;
-}
